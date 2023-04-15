e63d399bed1f2f89ca12eccf23b0107c9e80247a
tarun291 <tarunkumar222020@gmail.com>
2023-04-15 17:34:01 +0530

    add in repo

---
+++ client/src/components/Footer.js
@@ -0,0 +1,22 @@
+import "../styles/Footer.css";
+
+const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+
+  return (
+    <div className="footer">
+      <p>&copy;{year} Kevin Kuriakose</p>
+      <div className="links">
+        <a href="https://github.com/kevzpeter" rel="noreferrer" target="_blank">
+          <i className="fab fa-github-square"></i>
+        </a>
+        <a href="https://linkedin.com/in//kevinpeterk" rel="noreferrer" target="_blank">
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
